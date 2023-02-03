804ce9b04a63917bc33ecf516e26403243b08242
via.l0ft <vyacheslav.lednev@gmail.com>
2023-02-03 22:12:00 +0300

    Init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ouch-it-hurts
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ouch_it_hurts/core_test.clj
@@ -0,0 +1,7 @@
+(ns ouch-it-hurts.core-test
+  (:require [clojure.test :refer :all]
+            [ouch-it-hurts.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
