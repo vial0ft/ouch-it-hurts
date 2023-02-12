d836bd6e6efdd975c7ef7195641f1c4c6c938b9f
via.l0ft <vyacheslav.lednev@gmail.com>
2023-02-12 16:19:35 +0300

    Add logging and middleware with logging

---
+++ env/dev/clj/middlewares/dev_middlewares.clj
@@ -0,0 +1,31 @@
+(ns middlewares.dev-middlewares
+  (:require [clojure.tools.logging :as log]))
+
+
+(defn log-request [handler]
+  (fn [req]
+    (log/debug "Request:\n" req)
+    (handler req)))
+
+
+(defn log-response [handler]
+  (fn [req]
+    (let [resp (handler req)]
+      (log/debug "Response:\n" resp)
+      resp)))
+
+
+(defn log-request-response [handler]
+  (fn [req]
+    (let [resp (handler req)]
+      (log/debugf "\n>>> %s\n-------\n<<< %s" req resp)
+      resp)))
+
+
+
+
+
+
+
+
+
