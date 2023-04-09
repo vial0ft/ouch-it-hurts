2ef0d398948b6715ad6845cccae4153650d2e861
vial0ft <vyacheslav.lednev@gmail.com>
2023-04-09 23:01:58 +0300

    Add Promises for Api (#39)

--- src/cljs/ouch_it_hurts/components/patients_table_container.cljs
+++ src/cljs/ouch_it_hurts/components/patients_table_container.cljs
@@ -6,7 +6,8 @@
             [ouch-it-hurts.components.buttons-line.core :refer [ButtonsLine]]
             [ouch-it-hurts.components.modal.edit-patient-form :refer [EditPatientForm]]
             [ouch-it-hurts.components.modal.view-patient-form :refer [ViewPatientForm]]
-            [ouch-it-hurts.api :as api]))
+            [ouch-it-hurts.api :as api]
+            [ouch-it-hurts.utils.promises :as promises]))
 
 
 
@@ -17,74 +18,63 @@
 
 
 (defn- error-handler [app-state]
-  (fn[err]
-    (reset! (r/cursor app-state [:error]) {:ok? false
-                                           :message err})))
+    #(reset! (r/cursor app-state [:error]) {:ok? false :message %}))
 
+(defn- go-to-page [state pn] (reset! (r/cursor state [:paging :page-number]) pn))
 
-(defn- go-to-page [state pn]
-  (reset! (r/cursor state [:paging :page-number]) pn))
-
-(defn- result-handler [store app-state]
-  (fn [resp]
-   ;; (println "resp" resp)
-    (when-not @(r/cursor app-state [:error :ok?])
-      (reset! (r/cursor app-state [:error]) {:ok? true}))
-    (reset! store resp)))
+(defn- result-handler [store] #(reset! store %))
 
 (defn- fetch-patients-info [store app-state]
   (println "do fetch")
-  (let [filters-sorting (select-keys @app-state [:filters :sorting])
-        paging (:paging @app-state)]
-  (api/fetch-patients-info
-   (merge filters-sorting paging)
-   (result-handler store app-state)
-   (error-handler app-state))))
+  (when @(r/cursor app-state [:error :ok?])
+    (let [filters-sorting (select-keys @app-state [:filters :sorting])
+          paging (:paging @app-state)]
+      (-> (promises/retry #(api/fetch-patients-info (merge filters-sorting paging)) 5)
+          (.then (result-handler store))
+          (.catch (error-handler app-state))))
+    ))
 
 (defn- add-callback [app-state]
   (fn [patient-info]
-    (api/add-patient-info
-     patient-info
-     (fn [_] (go-to-page app-state 1))
-     (error-handler app-state))))
+    (-> (api/add-patient-info patient-info)
+        (.then #(go-to-page app-state 1))
+        (.catch (error-handler app-state)))
+    ))
+
 
 (defn- edit-callback [app-state]
   (fn [patients-info]
-    (api/update-patient-info
-     patients-info
-     (fn [_] (go-to-page app-state 1))
-     (error-handler app-state))
+    (-> (api/update-patient-info patients-info)
+     (.then #(go-to-page app-state 1))
+     (.catch (error-handler app-state)))
     ))
 
 (defn- delete-callback [app-state]
   (fn [ids]
     (println "to delete" ids)
-    (api/delete-patient-info
-     (first ids)
-     (fn [_] (go-to-page app-state 1))
-     (error-handler app-state))))
+    (-> (api/delete-patient-info (first ids))
+     (.then #(go-to-page app-state 1))
+     (.catch (error-handler app-state)))
+    ))
 
 (defn- view-callback [app-state]
   (fn [id modal-state edit-callback]
-    (api/get-patient-info-by-id
-     id
-     (fn [patient-info]
-       (println patient-info)
-       (reset! modal-state {:visible? true
-                            :form ViewPatientForm
-                            :args {:patient-info patient-info
-                                   :edit-callback edit-callback}
-                            }))
-     (error-handler app-state)
-     )))
+    (-> (api/get-patient-info-by-id id)
+        (.then #(do
+                  (println %)
+                  (reset! modal-state {:visible? true
+                                       :form ViewPatientForm
+                                       :args {:patient-info % :edit-callback edit-callback}})))
+        (.catch (error-handler app-state)))
+    ))
 
 (defn PatientsTableContainer [app-state]
   (let [
         selected-ids (r/atom {:ids {} :all-selected? false})
         !modal (r/atom {:visible? false})
-        fetch  (fn [patients-info app-state] (fetch-patients-info patients-info app-state))]
+        ]
     (fn [app-state]
-     (fetch patients-info app-state)
+      (fetch-patients-info patients-info app-state)
       [:div
        [FilterForm (r/cursor app-state [:filters])]
        [TableBlock
