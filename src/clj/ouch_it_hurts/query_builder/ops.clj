aaf8d62bb2edf3baa50329089ab7ea24aaca9930
vial0ft <vyacheslav.lednev@gmail.com>
2023-04-22 14:43:18 +0300

    Add in operator for query-builder (#44)

--- src/clj/ouch_it_hurts/query_builder/ops.clj
+++ src/clj/ouch_it_hurts/query_builder/ops.clj
@@ -1,9 +1,14 @@
 (ns ouch-it-hurts.query-builder.ops)
 
 
+(def supported-ops #{:between :is :in :or :and := :> :< :>= :<= :<>})
+
 (defn between [field from to]
   [field :between from :and to])
 
+(defn is-null
+  ([] nil)
+  ([key] [key :is :null]))
 
 (defn _or
   ([] nil)
@@ -34,6 +39,13 @@
 (defn neq [left right]
   [left :<> right])
 
-
-
-
+(defn in
+  ([] nil)
+  ([key seq]
+   (cond
+     (some nil? seq) (let [without-nil  (set (filter some? seq))
+                           count-not-nilables (count without-nil)]
+                       (if (pos? count-not-nilables)
+                         (->> (in key without-nil) (_or (is-null key)))
+                         (is-null key)))
+     :absent-nil [key :in seq])))
--- test/clj/ouch_it_hurts/query_builder_test.clj
+++ test/clj/ouch_it_hurts/query_builder_test.clj
@@ -6,7 +6,7 @@
 
 
 
-(deftest query-builder-test
+(deftest select-test
   (testing
       "select fragment"
     (is (= "select foo" (select :foo)))
@@ -15,58 +15,78 @@
     (is (= "select foo as bar, baz as \"qwe\"" (select :foo :as :bar :baz :as "\"qwe\"")))
     (is (= "select 1" (select 1)))
     )
+)
 
+(deftest from-test
   (testing
       "from fragment"
     (is (= "from table" (from [:table])))
     (is (= "from s.table" (from [:s/table])))
     (is (= "from table1 as t1, table2 as t2" (from [:table1 :as :t1 :table2 :as :t2])))
     )
+)
 
+(deftest where-test
   (testing
       "where fragment"
-    (is (= "where id = 1 or id = 2 or id = 3" (where (_or (eq :id 1) (eq  :id 2) (eq  :id 3)))))
-    (is (= "where id = 1 and id = 2 and id = 3" (where (_and (eq :id 1) (eq  :id 2) (eq  :id 3)))))
-    (is (= "where id = 1 and id = 2 and id = 3" (where (_and (eq :id 1) (eq  :id 2) (eq  :id 3)))))
-    (is (= "where id = 1 or id between 2 and 3" (where (_or (eq :id 1) (between :id 2 3)))))
-    )
+    (is (= "where (id = 1) or (id = 2) or (id = 3)" (where (_or (eq :id 1) (eq  :id 2) (eq  :id 3)))))
+    (is (= "where (id = 1) and (id = 2) and (id = 3)" (where (_and (eq :id 1) (eq  :id 2) (eq  :id 3)))))
+    (is (= "where (id = 1) or (id between 2 and 3)" (where (_or (eq :id 1) (between :id 2 3)))))
+    ))
 
+(deftest where-in
+  (testing
+      "where `in` with nil-free seq"
+    (is (= "where id in (1,2,3,4,5)" (where (in :id [1 2 3 4 5])))))
+
+  (testing
+      "where `in` with `nil` element in seq and one more not-nil element"
+    (is (= "where (id is null) or (id in (1))" (where (in :id [1 nil])))))
+
+  (testing
+      "where `in` with the only `nil` element in seq"
+    (is (= "where id is null" (where (in :id [nil])))))
+)
+
+(deftest ordering-test
   (testing
       "order by fragment"
     (is (= "order by id desc, date asc" (order-by [:id :desc :date :asc])))
-    )
+    ))
 
+(deftest limit-test
   (testing
       "limit fragment"
     (is (= "limit 100" (limit 100)))
-    )
+    ))
 
+(deftest offset-test
   (testing
       "offset fragment"
     (is (= "offset 100" (offset 100)))
-    )
+    ))
 
 
+(deftest complex-query-test
   (testing
       "complex query"
     (is (= (clojure.string/join " "
                                ["select id as \"qwe\""
                                 "from tablename as t, table2 as t2"
-                                "where id = 1 or date between 2023-01-01 and 2023-01-02"
+                                "where ((id = 1) or (date between '2023-01-01' and '2023-01-02'))"
+                                       "and ((name is null) or (name in ('John','Smith','Alex')))"
                                 "order by id desc"
                                 "offset 100 limit 100"]
                                 )
            (-> (select :id :as "\"qwe\"")
                (from :tablename :as :t :table2 :as :t2)
-               (where (_or (eq :id 1) (between
+               (where (_and (_or (eq :id 1) (between
                                                :date
-                                               (java.time.LocalDate/of 2023 01 01)
-                                               (java.time.LocalDate/of 2023 01 02))))
+                                               (str (java.time.LocalDate/of 2023 01 01))
+                                               (str (java.time.LocalDate/of 2023 01 02))))
+                            (in :name ["John" "Smith" "Alex" nil])))
                (order-by [:id :desc])
                (offset 100)
                (limit 100)
                )
-           )
-        )
-    )
-  )
+           ))))
