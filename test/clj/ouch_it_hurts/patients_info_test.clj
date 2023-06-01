242d3866cd5f89f9da5057d6e041ba7d0927db2a
via.l0ft <vyacheslav.lednev@gmail.com>
2023-06-01 11:39:43 +0300

    test data

--- test/clj/ouch_it_hurts/patients_info_test.clj
+++ test/clj/ouch_it_hurts/patients_info_test.clj
@@ -396,3 +396,74 @@
     (is (spec/valid? :ouch-it-hurts.specs/restore-patient-response (service/restore-patient-info id)))))
 
 
+
+(comment
+
+(defn m-gen []
+  (let [first-name (gen/generate (spec/gen (spec/nilable #{"Иван" "Сергей" "Георгий" "Александр" "Михаил"})))
+        last-name (gen/generate (spec/gen (spec/nilable #{"Петров" "Сидоров" "Иванов" "Александров" })))
+        middle-name (gen/generate (spec/gen (spec/nilable #{"Иванович" "Сергеевич" "Георгиевич" "Александрович" "Михаилович"})))]
+    {:first-name first-name
+     :middle-name middle-name
+     :last-name last-name}))
+
+(defn f-gen []
+  (let [first-name (gen/generate (spec/gen (spec/nilable #{"Светлана" "Екатерина" "Ксения" "Людмила" "Маргарита"})))
+        last-name (gen/generate (spec/gen (spec/nilable #{"Петрова" "Сидорова" "Иванова" "Александрова"})))
+        middle-name (gen/generate (spec/gen (spec/nilable #{"Ивановна" "Сергеевна" "Георгиевна" "Александровна" "Михаиловна"})))]
+    {:first-name first-name
+     :middle-name middle-name
+     :last-name last-name}))
+
+ (defn sex-gen []
+   (gen/generate (spec/gen (spec/nilable #{"male" "female"}))))
+
+ (defn date-gen []
+   (let [from #inst "1970"
+         to #inst "2020"
+         from-local-str (.toString (inst->localdate from))
+         to-local-str (.toString (inst->localdate to))
+         dates (gen-date 10 from to)]
+     (gen/generate (spec/gen (spec/nilable (into #{} dates))))))
+
+ (defn flat-gen []
+   (gen/generate (spec/gen (into #{""} (mapv #(str "кв. " %)  (range 1 100))))))
+
+ (defn building-gen []
+   (gen/generate (spec/gen (into #{""} (mapv #(str "д. " %)  (range 1 100))))))
+
+ (defn street-type-gen []
+   (gen/generate (spec/gen #{"ул." "пл." "пр." "пер."})))
+
+ (defn street-name-gen []
+   (gen/generate (spec/gen #{"Ленина" "Пушкина" "Гоголя" "Булгакова" "Есенина" "Менделеева" "Попова" "Дзержинского"})))
+
+ (defn city-gen []
+   (gen/generate (spec/gen #{"Москва" "Санкт-Петербург" "Казань" "Нижний Новгород" "Екатеринбург" "Владивосток" "Калининград"})))
+
+ (defn street-gen []
+   (str (street-type-gen) (street-name-gen)))
+
+ (defn address-gen []
+   (gen/generate
+    (spec/gen
+     (spec/nilable
+      (into #{}
+            (map (fn [_] (s/join ", " [(city-gen) (street-gen) (building-gen) (flat-gen)]))) (range 10))))))
+
+
+ (defn patient-gen []
+   (-> (if-let [s (sex-gen)]
+         (-> (if (= s "male") (m-gen) (f-gen)) (assoc :sex s))
+         ((gen/generate (spec/gen #{m-gen f-gen}))))
+       (assoc :address (address-gen))
+       (assoc :oms (tg/oms-gen))
+       (assoc :birth-date (date-gen))))
+
+
+
+
+ (doseq [i (range 100)]
+     (service/add-patient-info (patient-gen)))
+
+)
