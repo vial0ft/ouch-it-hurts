be3bb832b0311f11795d48c304dae1ae3699596c
via.l0ft <vyacheslav.lednev@gmail.com>
2023-06-12 22:46:12 +0300

    remove useless code

--- src/cljs/ouch_it_hurts/components/patients_table_container.cljs
+++ src/cljs/ouch_it_hurts/components/patients_table_container.cljs
@@ -6,91 +6,13 @@
             [ouch-it-hurts.components.modal.core :refer [PatientModal]]
             [ouch-it-hurts.components.buttons-line.core :refer [ButtonsLine]]
             [ouch-it-hurts.components.modal.edit-patient-form :refer [EditPatientForm]]
-            [ouch-it-hurts.components.modal.view-patient-form :refer [ViewPatientForm]]
-            [ouch-it-hurts.api :as api]
-            [ouch-it-hurts.specs :as specs]
-            [ouch-it-hurts.utils.promises :as promises]
-            [clojure.spec.alpha :as s]
-            [clojure.string :refer [join]]
-            [spec-tools.core :as st]))
-
-(def patients-info (r/atom {:selected-ids {:ids {}
-                                           :all-selected? false}}))
-
-;; -------------------------
-;; States
-
-(defn- error-handler [app-state]
-  #(reset! (r/cursor app-state [:error]) {:ok? false
-                                          :message (str (if-some [err (:parse-error %)]
-                                                          (select-keys err [:status-text :original-text])
-                                                          %))}))
-
-(defn- go-to-page [state pn] (reset! (r/cursor state [:paging :page-number]) pn))
-
-(defn- handle-fetch-result [store]
-  #(reset! store {:ids {} :selected-all false :table-info %}))
-
-(defn- fetch-patients-info [store app-state]
-  (when @(r/cursor app-state [:error :ok?])
-    (let [[result details] (specs/confirm-if-valid
-                            :ouch-it-hurts.specs/query-request
-                            (select-keys @app-state [:filters :sorting :paging]))]
-      (case result
-        :ok (-> (promises/retry #(api/fetch-patients-info details) 5)
-                (.then (handle-fetch-result store))
-                (.catch (error-handler app-state)))
-        (reset! (r/cursor app-state [:error])  {:ok? false
-                                                :message (join "\n" details)})))))
-
-(defn post-update-action [app-state store]
-  #(if-not (= @(r/cursor app-state [:paging :page-number]) 1)
-     (go-to-page app-state 1)
-     (fetch-patients-info store app-state)))
-
-(defn- add-callback [app-state store]
-  (fn [patient-info]
-    (-> (api/add-patient-info patient-info)
-        (.then (post-update-action app-state store))
-        (.catch (error-handler app-state)))))
-
-(defn- edit-callback [app-state store]
-  (fn [patients-info]
-    (-> (api/update-patient-info patients-info)
-        (.then (post-update-action app-state store))
-        (.catch (error-handler app-state)))))
-
-(defn- delete-callback [app-state store]
-  (fn [ids]
-    (-> (map api/delete-patient-info ids)
-        (js/Promise.all)
-        (.then (post-update-action app-state store))
-        (.catch (error-handler app-state)))))
-
-(defn- restore-callback [app-state store]
-  (fn [id]
-    (println "restore " id)
-    (-> (api/restore-patient-info id)
-        (.then (post-update-action app-state store))
-        (.catch (error-handler app-state)))))
-
-(defn- view-callback [app-state]
-  (fn [id modal-state edit-callback restore-callback]
-    (-> (api/get-patient-info-by-id id)
-        (.then #(reset! modal-state {:visible? true
-                                     :form ViewPatientForm
-                                     :args {:patient-info %
-                                            :edit-callback edit-callback
-                                            :restore-callback restore-callback}}))
-        (.catch (error-handler app-state)))))
+            [ouch-it-hurts.components.modal.view-patient-form :refer [ViewPatientForm]]))
 
 (defn PatientsTableContainer []
-  (let [!modal (r/atom {:visible? false})]
     (fn []
       (let [patients-info @(subscribe [:table/info])]
         [:div.patient-table-container
          [FilterForm]
          [TableBlock patients-info]
          [ButtonsLine (get-in patients-info [:patients :data])]
-         [PatientModal]
-         ]))))
+         [PatientModal]])))
