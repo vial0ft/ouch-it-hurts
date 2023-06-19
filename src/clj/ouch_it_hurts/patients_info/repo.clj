97a34217135f20c6d4c8871be5a8e2396b0c33f9
via.l0ft <vyacheslav.lednev@gmail.com>
2023-06-19 09:01:25 +0300

    Add honeysql and remove custom qubi

--- src/clj/ouch_it_hurts/patients_info/repo.clj
+++ src/clj/ouch_it_hurts/patients_info/repo.clj
@@ -1,13 +1,9 @@
 (ns ouch-it-hurts.patients-info.repo
   (:require
-   [ouch-it-hurts.query-builder.core :as qb]
-   [ouch-it-hurts.query-builder.ops :as ops]
    [ouch-it-hurts.query-builder.utils :as qb-utils]
    [next.jdbc :as jdbc]
-   [next.jdbc.result-set :as rs]
-   [next.jdbc.sql :as sql]
-   [next.jdbc.prepare :as p]
-   [next.jdbc.date-time]))
+   [next.jdbc.date-time]
+   [honey.sql :as sql]))
 
 (defn- patient-info-mapper [info]
   (if (:birth-date info)
@@ -15,57 +11,62 @@
     info))
 
 (defn get-info-by-id [ds id]
-  (let [result (sql/get-by-id @ds :patients.info id jdbc/unqualified-snake-kebab-opts)]
+  (let [query (-> {:select [:*] :from [:patients/info] :where [:= :id id]} sql/format)
+        result (jdbc/execute-one! @ds query jdbc/unqualified-snake-kebab-opts)]
     (when result (patient-info-mapper result))))
 
 (defn get-info-by-oms [ds oms]
-  (let [result (first (sql/find-by-keys @ds :patients.info {:oms oms} jdbc/unqualified-snake-kebab-opts))]
+  (let [query (-> {:select [:*] :from [:patients/info] :where [:= :oms oms]} sql/format)
+        result (jdbc/execute-one! @ds query jdbc/unqualified-snake-kebab-opts)]
     (when result (patient-info-mapper result))))
 
 (defn query-infos [ds {:keys [offset limit filters sorting]}]
-  (let [condition (qb-utils/map->where ops/_and  filters qb-utils/as-snake-name)
-        [query & args]       (-> (qb/select [:*])
-                                 (qb/from [:patients.info])
-                                 (qb/where [condition])
-                                 (qb/order-by [(qb-utils/map->order-by sorting qb-utils/as-snake-name)])
-                                 (qb/offset offset)
-                                 (qb/limit limit)
-                                 (qb/build))
-        [total-query & total-args] (-> (qb/select [(ops/_count)])
-                                       (qb/from [:patients.info])
-                                       (qb/where [condition])
-                                       (qb/build))]
+  (let [condition (qb-utils/map->where :and filters)
+        order-by (qb-utils/map->order-by sorting)
+        query     (-> {:select [:*]
+                       :from [:patients/info]
+                       :where condition
+                       :order-by order-by
+                       :offset offset
+                       :limit limit}
+                      sql/format)
+        total-query (-> {:select [:%count.*]
+                         :from [:patients/info]
+                         :where [condition]}
+                        sql/format)]
     (jdbc/with-transaction [tx @ds]
-      (let [ps (jdbc/prepare tx [query])
-            result (jdbc/execute! (p/set-parameters ps args) nil {:builder-fn rs/as-unqualified-kebab-maps})
-            total-ps (jdbc/prepare tx [total-query])
-            [{:keys [count]}] (jdbc/execute! (p/set-parameters total-ps total-args))]
+      (let [result (jdbc/execute! tx query jdbc/unqualified-snake-kebab-opts)
+            [{:keys [count]}] (jdbc/execute! tx total-query)]
         {:data (when result (map patient-info-mapper result))
          :total count}))))
 
 (defn insert-info [ds new-patient-info]
-  (-> (sql/insert! @ds :patients.info
-                   (-> new-patient-info
-                       (select-keys
-                        [:first-name
-                         :last-name
-                         :middle-name
-                         :birth-date
-                         :sex
-                         :address
-                         :oms]))
-                   jdbc/unqualified-snake-kebab-opts)
-      (patient-info-mapper)))
+  (let [query (-> {:insert-into [:patients/info]
+                   :values [(-> new-patient-info
+                                (select-keys
+                                 [:first-name
+                                  :last-name
+                                  :middle-name
+                                  :birth-date
+                                  :sex
+                                  :address
+                                  :oms]))]
+                   :returning :*}
+                  (sql/format))]
+    (patient-info-mapper (jdbc/execute-one! @ds query jdbc/unqualified-snake-kebab-opts))))
 
 (defn set-deleted [ds id deleted?]
-  (-> (sql/update! @ds :patients.info
-                   {:deleted deleted?
-                    :updated-at (.toInstant (java.time.OffsetDateTime/now))}
-                   {:id id :deleted (not deleted?)} jdbc/unqualified-snake-kebab-opts)
-      (:next.jdbc/update-count)))
+  (let [query (-> {:update [:patients/info]
+                   :set {:deleted deleted?
+                         :updated-at (.toInstant (java.time.OffsetDateTime/now))}
+                   :where [:and [:= :id id] [:= :deleted (not deleted?)]]}
+                  (sql/format))]
+  (->  (jdbc/execute-one! @ds query jdbc/unqualified-snake-kebab-opts)
+       (:next.jdbc/update-count))))
 
 (defn update-info [ds id patient-info-for-update]
-  (let [info-for-update (-> (select-keys patient-info-for-update
+  (let [query (-> {:update :patients/info
+                   :set (-> (select-keys patient-info-for-update
                                          [:first-name
                                           :last-name
                                           :middle-name
@@ -74,6 +75,8 @@
                                           :address
                                           :updated-at
                                           :oms])
-                            (assoc :updated-at (.toInstant (java.time.OffsetDateTime/now))))]
-    (-> (sql/update! @ds :patients.info info-for-update {:id id :deleted false} jdbc/unqualified-snake-kebab-opts)
-        (:next.jdbc/update-count))))
+                            (assoc :updated-at (.toInstant (java.time.OffsetDateTime/now))))
+                   :where [:= :id id]}
+                  sql/format)]
+    (->  (jdbc/execute-one! @ds query jdbc/unqualified-snake-kebab-opts)
+         (:next.jdbc/update-count))))
--- src/clj/ouch_it_hurts/query_builder/ops.clj
+++ src/clj/ouch_it_hurts/query_builder/ops.clj
@@ -1,56 +1,46 @@
 (ns ouch-it-hurts.query-builder.ops)
 
-(def supported-ops #{:between :is :in :or :and := :> :< :>= :<= :<> :like})
-
-(def open (symbol "("))
-
-(def close (symbol ")"))
-
-(defn scoped [expr]
-  (into [] (cons open (conj expr close))))
-
-(defn is-null
-  ([] [nil])
-  ([key] (scoped [key :is :null])))
-
 (defn _or
   ([] nil)
   ([single] [single])
-  ([first & seq] (scoped (reduce (fn [acc el] (conj acc :or el)) [first] seq))))
+  ([first seq] (reduce (fn [acc el] (conj acc el)) [:and] (into [] seq))))
 
 (defn _and
   ([] nil)
   ([single] [single])
-  ([first & seq] (scoped (reduce (fn [acc el] (conj acc :and el)) [first] seq))))
+  ([first seq] (reduce (fn [acc el] (conj acc el)) [:and] (into [] seq))))
+
+(defn is-null
+  ([] [nil])
+  ([key] [:is key nil]))
 
 (defn eq [left right]
-  (scoped [left := right]))
+  [:= left right])
 
 (defn lt [left right]
-  (scoped [left :< right]))
+  [:< left right])
 
 (defn gt [left right]
-  (scoped [left :> right]))
+  [:> left right])
 
 (defn lte [left right]
-  (scoped [left :<= right]))
+  [:<= left right])
 
 (defn gte [left right]
-  (scoped [left :>= right]))
+  [:>= left right])
 
 (defn neq [left right]
-  (scoped [left :<> right]))
+  [:<> left right])
 
-(defn _any
-  ([] nil)
-  ([key seq]
-   (cond
-     (some nil? seq) (let [without-nil  (set (filter some? seq))
-                           count-not-nilables (count without-nil)]
-                       (if (pos? count-not-nilables)
-                         (->> (_any key without-nil) (_or (is-null key)))
-                         (is-null key)))
-     :absent-nil (scoped [key := (symbol "any(") (into-array seq) (symbol ")")]))))
+(defn _in
+  [key seq]
+  (cond
+    (some nil? seq) (let [without-nil  (set (filter some? seq))
+                          count-not-nilables (count without-nil)]
+                      (if (pos? count-not-nilables)
+                        [:or (_in key without-nil) (is-null key)]
+                        (is-null key)))
+    :absent-nil [:in key seq]))
 
 (defn between
   ([field {:keys [from to]}] (between field from to))
@@ -59,11 +49,7 @@
      [true true] nil
      [true false] (lte field to)
      [false true] (gte field from)
-     (scoped [field :between from :and to]))))
+     [:between field from to])))
 
-(defn like [field {:keys [pattern]}] (scoped [field :like pattern]))
+(defn like [field {:keys [pattern]}] [:like field pattern])
 
-
-(defn _count
-  ([] (_count [:*]))
-  ([columns] (into [] (cons (symbol "count(") (conj columns close)))))
