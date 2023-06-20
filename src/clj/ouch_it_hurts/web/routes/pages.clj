e441d7473e772b491aa98b4266c74ebf242fe8a5
via.l0ft <vyacheslav.lednev@gmail.com>
2023-06-20 20:40:11 +0300

    remove pages

--- src/clj/ouch_it_hurts/web/routes/pages.clj
+++
@@ -1,9 +0,0 @@
-(ns ouch-it-hurts.web.routes.pages
-  (:require [ouch-it-hurts.web.http-responses.core :as http-resp]
-            [clojure.java.io :as io]))
-
-(defn- home [_]
-  (http-resp/page (slurp (io/resource "public/index.html"))))
-
-(defn pages []
-  [["/" {:get {:handler home}}]])
